88d281709e0915d029a7e34f18b8dd8a6efb670d
Niklas Nilsson <potentialforanything@gmail.com>
2021-11-19 20:37:47 +0100

    Integrated database support in api. Added main function entrypoint. Renamed app to chatbotapi.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,29 @@
 from typing import Optional
 from fastapi import Body, FastAPI
-from models import ChatItem
+from models import ChatItem, DialogItem
+from mongo_service import MongoService
+from distutils import util
+import uvicorn
 
-app = FastAPI()
+chatbotapi = FastAPI()
+storageProvider = MongoService()
 
-@app.post("/data/{customerId}/{dialogId}")
+@chatbotapi.post("/data/{customerId}/{dialogId}")
 async def push_dialog(customerId: str, dialogId: str, chatItem: ChatItem):
-    return {"method": "post", "customerId": customerId, "dialogId": dialogId, "item_text": chatItem.text, "item_language": chatItem.language}
+    acknowledged = storageProvider.insert(DialogItem(customerId, dialogId, chatItem.text, chatItem.language))
+    return {"acknowledged": acknowledged}
 
-@app.post("/consents/{dialogId}")
+@chatbotapi.post("/consents/{dialogId}")
 async def push_consent(dialogId: str, consent: str = Body(None, regex="^true$|^false$")):
-    return {"method": "post", "dialogId": dialogId, "consent": consent}
+    acknowledged = True
+    if (not util.strtobool(consent)):
+        acknowledged = storageProvider.delete_customer_dialogs(dialogId)
+    return {"acknowledged": acknowledged}
 
-@app.get("/data/")
-async def read_items(language: Optional[str] = None, customerId: Optional[str] = None):
-    results = {}
-    if language:
-        results.update({"language": language})
-    if customerId:
-        results.update({"customerId": customerId})
-    return results
+@chatbotapi.get("/data/")
+async def read_dialogs(language: Optional[str] = None, customerId: Optional[str] = None, bookmark: Optional[str] = None):
+    results, bookmark = storageProvider.get(customerId, language, bookmark)
+    return {"results": results, "bookmark": bookmark}
+
+if __name__ == "__main__":
+    uvicorn.run(chatbotapi, host="0.0.0.0", port=8000)
